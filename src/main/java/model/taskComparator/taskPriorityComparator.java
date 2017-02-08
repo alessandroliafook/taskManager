76a2e735837162ed7a733a07c5ab388bffa3f2b1
alessandroliafook <alessandro.liafook@gmail.com>
2017-02-08 09:01:41 -0300

    Adjust comparator by priority

--- src/main/java/model/taskComparator/taskPriorityComparator.java
+++ src/main/java/model/taskComparator/taskPriorityComparator.java
@@ -10,6 +10,20 @@ import java.util.Comparator;
 public class taskPriorityComparator  implements Comparator<Task> {
 	@Override
 	public int compare(Task o1, Task o2) {
-		return 0;
+
+		if (o1.getPriority().toLowerCase().equals("alta"))
+			return 1;
+
+		else if (o2.getPriority().toLowerCase().equals("alta"))
+			return -1;
+
+		else if (o1.getPriority().toLowerCase().equals("media"))
+			return 1;
+
+		else if (o2.getPriority().toLowerCase().equals("media"))
+			return -1;
+
+		else
+			return 1;
 	}
 }
